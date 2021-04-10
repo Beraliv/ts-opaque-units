da8855bc56d2e4480074600175e780bee50233cb
Alexey Berezin <beraliv.spb@gmail.com>
2021-04-10 15:46:33 +0200

    feat: ➕ add DAYS_IN_MONTHS

---
+++ internal/time/constants/DAYS_IN_MONTHS.ts
@@ -0,0 +1,55 @@
+import { MonthName } from "../types/MonthName";
+
+export const DAYS_IN_MONTHS: {
+  day: number;
+  month: MonthName;
+}[] = [
+  {
+    day: 31,
+    month: "January",
+  },
+  {
+    day: 28,
+    month: "February",
+  },
+  {
+    day: 31,
+    month: "March",
+  },
+  {
+    day: 30,
+    month: "April",
+  },
+  {
+    day: 31,
+    month: "May",
+  },
+  {
+    day: 30,
+    month: "June",
+  },
+  {
+    day: 31,
+    month: "July",
+  },
+  {
+    day: 31,
+    month: "August",
+  },
+  {
+    day: 30,
+    month: "September",
+  },
+  {
+    day: 31,
+    month: "October",
+  },
+  {
+    day: 30,
+    month: "November",
+  },
+  {
+    day: 31,
+    month: "December",
+  },
+];
