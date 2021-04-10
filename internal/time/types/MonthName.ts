9fcc0a4a245121fe6e817d20d0cce2a029fccccf
Alexey Berezin <beraliv.spb@gmail.com>
2021-04-10 15:46:23 +0200

    feat: ➕ add MonthName

---
+++ internal/time/types/MonthName.ts
@@ -0,0 +1,13 @@
+export type MonthName =
+  | "January"
+  | "February"
+  | "March"
+  | "April"
+  | "May"
+  | "June"
+  | "July"
+  | "August"
+  | "September"
+  | "October"
+  | "November"
+  | "December";
