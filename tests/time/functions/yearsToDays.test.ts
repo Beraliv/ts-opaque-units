da736e5a15e0682cf3d8d0713e41627b3c51c6b6
Alexey Berezin <beraliv.spb@gmail.com>
2021-04-10 15:15:09 +0200

    feat: ➕ add float test case for yearsToDays

---
+++ tests/time/functions/yearsToDays.test.ts
@@ -0,0 +1,53 @@
+import { yearsToDays } from "../../../src/time/functions/yearsToDays";
+import { Days } from "../../../src/time/types/Days";
+import { Years } from "../../../src/time/types/Years";
+
+describe(yearsToDays.name, () => {
+  test("returns 0 for 0", () => {
+    const actual = yearsToDays(0 as Years);
+    const expected = 0 as Days;
+
+    expect(actual).toEqual<typeof actual>(expected);
+  });
+
+  test("returns 365.25 for 1 year without passing start date", () => {
+    const actual = yearsToDays(1 as Years);
+    const expected = 365.25 as Days;
+
+    expect(actual).toEqual<typeof actual>(expected);
+  });
+
+  test("returns -365.25 for 1 year ago without passing start date", () => {
+    const actual = yearsToDays(-1 as Years);
+    const expected = -365.25 as Days;
+
+    expect(actual).toEqual<typeof actual>(expected);
+  });
+
+  test("returns 183 for 0.5 year from 2020 (leap year)", () => {
+    const actual = yearsToDays(0.5 as Years, {
+      year: 2020 as Years,
+    });
+    const expected = 183 as Days;
+
+    expect(actual).toEqual<typeof actual>(expected);
+  });
+
+  test("returns 366 for 1 year from 2020 (leap year)", () => {
+    const actual = yearsToDays(1 as Years, {
+      year: 2020 as Years,
+    });
+    const expected = 366 as Days;
+
+    expect(actual).toEqual<typeof actual>(expected);
+  });
+
+  test("returns 365 for 1 year from 2021", () => {
+    const actual = yearsToDays(1 as Years, {
+      year: 2021 as Years,
+    });
+    const expected = 365 as Days;
+
+    expect(actual).toEqual<typeof actual>(expected);
+  });
+});
