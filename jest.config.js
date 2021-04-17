ea342e5d908f04308344e740c469d571b87f1977
Alexey Berezin <beraliv.spb@gmail.com>
2021-04-17 16:06:28 +0200

    test: ➕ add coverageThreshold 100 for everything

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
   transform: {
     "^.+\\.(ts)$": "ts-jest",
   },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
